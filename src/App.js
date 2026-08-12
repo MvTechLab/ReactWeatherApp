61b47ac4765a29fc4f4819a5fa7318081849d474
Manveer Singh <developmentmy18@gmail.com>
2026-08-12 10:39:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import CityComponent from "./modules/CityComponent";
 import WeatherComponent from "./modules/WeatherInfoComponent";
 
 export const WeatherIcons = {
-  "01d": "/react-weather-app/icons/sunny.svg",
-  "01n": "/react-weather-app/icons/night.svg",
-  "02d": "/react-weather-app/icons/day.svg",
-  "02n": "/react-weather-app/icons/cloudy-night.svg",
-  "03d": "/react-weather-app/icons/cloudy.svg",
-  "03n": "/react-weather-app/icons/cloudy.svg",
-  "04d": "/react-weather-app/icons/perfect-day.svg",
-  "04n": "/react-weather-app/icons/cloudy-night.svg",
-  "09d": "/react-weather-app/icons/rain.svg",
-  "09n": "/react-weather-app/icons/rain-night.svg",
-  "10d": "/react-weather-app/icons/rain.svg",
-  "10n": "/react-weather-app/icons/rain-night.svg",
-  "11d": "/react-weather-app/icons/storm.svg",
-  "11n": "/react-weather-app/icons/storm.svg",
+  "01d": "/ReactWeatherApp/icons/sunny.svg",
+  "01n": "/ReactWeatherApp/icons/night.svg",
+  "02d": "/ReactWeatherApp/icons/day.svg",
+  "02n": "/ReactWeatherApp/icons/cloudy-night.svg",
+  "03d": "/ReactWeatherApp/icons/cloudy.svg",
+  "03n": "/ReactWeatherApp/icons/cloudy.svg",
+  "04d": "/ReactWeatherApp/icons/perfect-day.svg",
+  "04n": "/ReactWeatherApp/icons/cloudy-night.svg",
+  "09d": "/ReactWeatherApp/icons/rain.svg",
+  "09n": "/ReactWeatherApp/icons/rain-night.svg",
+  "10d": "/ReactWeatherApp/icons/rain.svg",
+  "10n": "/ReactWeatherApp/icons/rain-night.svg",
+  "11d": "/ReactWeatherApp/icons/storm.svg",
+  "11n": "/ReactWeatherApp/icons/storm.svg",
 };
 
 const Container = styled.div`
